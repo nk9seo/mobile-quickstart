c624f2c953e969e456ab83a625927805c3675312
Moris Finkel <moris@moris.org.ua>
2015-04-29 18:22:48 +0300

    dump from

--- server.py
+++ server.py
@@ -60,8 +60,6 @@ def call():
   parseJson = json.loads(checkNumber.text)
   # to = parseJson['number']
 
-  return str(parseJson)
-
   if not from_client:
     # PSTN -> client
     resp.dial(callerId=from_value).client(to)
