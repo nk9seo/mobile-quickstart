9e530e9d45b6104f31eed82ab3cabf2f57899d0a
Moris Finkel <moris@moris.org.ua>
2015-04-29 18:20:12 +0300

    dump from

--- server.py
+++ server.py
@@ -54,7 +54,7 @@ def call():
   from_client = from_value.startswith('client')
   caller_id = os.environ.get("CALLER_ID", from_client)
 
-  params =  {'number': to, 'sign': SIGN}
+  params =  {'number': to.strip(), 'sign': SIGN}
   checkNumber = requests.get(SERVER_URL, params=params)
 
   parseJson = json.loads(checkNumber.text)
